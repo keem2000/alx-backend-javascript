1109864601a8780b39053e5b26836e4132d16351
keem2000 <joycelinaomari@gmail.com>
2023-04-27 07:41:39 +0000

    manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function getListStudents() {
+    const obj = [
+	    { firstName:e', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return obj;
+}
+
+export default getListStudents;
---
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -0,0 +1,9 @@
+import getListStudents from './0-get_list_students.js';
+import updateStudentGradeByCity from './4-update_grade_by_city.js';
+
+console.log(
+    updateStudentGradeByCity(getListStudents(), 'San Francisco', [
+	    {.log( updateStudentGradeByCity(( [
+		studentId: 5, grade: 97 },
+					    ])
+		 );
