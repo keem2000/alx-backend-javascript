9dc0271d016ac55c0b842962a34685fb3ed18500
keem2000 <joycelinaomari@gmail.com>
2023-04-28 17:44:51 +0000

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,5 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-	/*eslint-disable */
-	if (true) {
-	    resolve();
-	} else {
-	    reject();
-	}
-	/* eslint-enable */
-    });
+/* eslint-disable */
+export default function getResponseFromAPI() {
+        return new Promise((resolve, reject) => {});
 }
 
-export default getResponseFromAPI;
